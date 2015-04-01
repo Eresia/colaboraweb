4dd785cc0ec0cf17a80effcff5cbc4f45b8fd8b2
Eresia <bastien.lepesant@gmail.com>
2015-04-01 17:20:17 +0200

    Correction pour MAMP/LAMP (1)

--- login/confirm_subscription.php
+++ login/confirm_subscription.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	define("HTTP_ROOT", ($_SERVER['HTTP_HOST'] == "localhost")?"http://localhost/colaboraweb":"http://".$_SERVER['HTTP_HOST']);
+	define("HTTP_ROOT", (strstr($_SERVER['HTTP_HOST'], 'localhost') != false)?"http://localhost/colaboraweb":"http://".$_SERVER['HTTP_HOST']);
 	define("SERV_ROOT", ($_SERVER['HTTP_HOST'] == "localhost")?$_SERVER['DOCUMENT_ROOT']."/colaboraweb":$_SERVER['DOCUMENT_ROOT']);
 	require_once(SERV_ROOT."/function/function_login.php");
 	if(!isset($_POST['pseudo'])){
