086463215516d8a5d99ea3e2c8725b6a83865636
Eresia <bastien.lepesant@gmail.com>
2015-04-01 17:41:56 +0200

    Correction pour MAMP/LAMP (3)

--- login/login.php
+++ login/login.php
@@ -1,6 +1,6 @@
 <?php 
     session_start();
-	define("HTTP_ROOT", (strstr($_SERVER['HTTP_HOST'], 'localhost') != false)?"http://localhost/colaboraweb":"http://".$_SERVER['HTTP_HOST']);
+	define("HTTP_ROOT", (strstr($_SERVER['HTTP_HOST'], 'localhost') != false)?"http://".$_SERVER['HTTP_HOST']."/colaboraweb":"http://".$_SERVER['HTTP_HOST']);
 	define("SERV_ROOT", (strstr($_SERVER['HTTP_HOST'], 'localhost') != false)?$_SERVER['DOCUMENT_ROOT']."/colaboraweb":$_SERVER['DOCUMENT_ROOT']);
     require_once(SERV_ROOT."/function/base.php");
 	require_once(SERV_ROOT."/function/function_login.php");
