a74e9252e7b2775b6860446eec74e6be66dfa23a
Eresia <bastien.lepesant@gmail.com>
2015-04-05 16:14:01 +0200

    Redéfinition des defines mysql

--- define/mysql_define.php
+++ define/mysql_define.php
@@ -1,6 +1,13 @@
 <?php
-	define("DTB_LINK", (strstr($_SERVER['HTTP_HOST'], 'localhost') != false)?"localhost":"mysql.hostinger.fr");
-	define("DTB_USER", (strstr($_SERVER['HTTP_HOST'], 'localhost') != false)?"root":"u590490691_sinep");
-	define("DTB_PASS", (strstr($_SERVER['HTTP_HOST'], 'localhost') != false)?"":"sinepsinep");
-	define("DTB_NAME", (strstr($_SERVER['HTTP_HOST'], 'localhost') != false)?"colaboraweb":"u590490691_colab");
+	//Base de donnée local
+	define("DTB_LINK", "localhost");
+	define("DTB_USER", "root");
+	define("DTB_PASS", "");
+	define("DTB_NAME", "colaboraweb");
+	
+	//Base de donnée en ligne
+	//define("DTB_LINK", "mysql.hostinger.fr");
+	//define("DTB_USER", "u590490691_sinep");
+	//define("DTB_PASS", "sinepsinep");
+	//define("DTB_NAME", "u590490691_colab");
 ?>
