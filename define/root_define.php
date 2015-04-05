3dfcc6aec9de6ce767ddc019536097100dd2fc8d
Eresia <bastien.lepesant@gmail.com>
2015-04-05 16:08:15 +0200

    Redéfinition un peu plus propre des constantes roots. Ne pas écraser le fichier root_define.php en ligne !!!

---
+++ define/root_define.php
@@ -0,0 +1,9 @@
+<?php
+	//Define local
+	define("HTTP_ROOT", "http://".$_SERVER['HTTP_HOST']."/colaboraweb");
+	define("SERV_ROOT", $_SERVER['DOCUMENT_ROOT']."/colaboraweb");
+	
+	//Define site
+	//define("HTTP_ROOT", "http://".$_SERVER['HTTP_HOST']);
+	//define("SERV_ROOT", $_SERVER['DOCUMENT_ROOT']);
+?>
--- login/disconnect.php
+++ login/disconnect.php
@@ -1,7 +1,6 @@
 <?php
     session_start();
-	define("HTTP_ROOT", (strstr($_SERVER['HTTP_HOST'], 'localhost') != false)?"http://".$_SERVER['HTTP_HOST']."/colaboraweb":"http://".$_SERVER['HTTP_HOST']);
-	define("SERV_ROOT", (strstr($_SERVER['HTTP_HOST'], 'localhost') != false)?$_SERVER['DOCUMENT_ROOT']."/colaboraweb":$_SERVER['DOCUMENT_ROOT']);
+	require_once("../define/root_define.php");
 	require_once(SERV_ROOT."/function/function_login.php");
     disconnect();
 	header('Location: '.HTTP_ROOT.'/index.php');
